e72ac13cf53adb0726b3e32591cf25ca675cf041
BytteL <lettybedard@gmail.com>
2020-04-11 19:12:54 -0400

    list display working on edit page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const groceryCont = require("../controller/groceryController");
+const groceryController = require("../controller/groceryController");
 
 // router.get("/", function (req, res) {
 //   res.render("index.handlebars");
@@ -16,17 +16,17 @@ router.get("/signin", function (req, res) {
 
 router.get("/list", function (req, res) {
   const hhID = 1; //dynamic later
-  groceryCont.allByHousehold(hhID).then((response) => {
+  groceryController.allByHousehold(hhID).then((response) => {
     var hbsObject = {
       listItem: response,
     };
-    console.log(response);
+    //console.log(response);
     res.render("list.handlebars", hbsObject);
   });
 });
 
 router.get("/", function (req, res) {
-  groceryCont.allItems().then((response) => {
+  groceryController.allItems().then((response) => {
     var hbsObject = {
       Item: response,
     };
@@ -34,13 +34,22 @@ router.get("/", function (req, res) {
   });
 });
 
-router.get("/edit", function (req, res) {
-  groceryCont.allItems().then((response) => {
-    var hbsObject = {
-      Item: response,
-    };
-    res.render("index.handlebars", hbsObject);
-  });
-});
+router.get("/edit", async function (req, res) {
+  const hhID = 1; //dynamic later
+  const listItems = await groceryController.allByHousehold(hhID);
+  //console.log(`LINE 40 LIST ITEMS: ${listItems}`);
+  
+  const items = await groceryController.allItems();
+  //console.log(`LINE 43 ALL ITEMS: ${items}`);
+    
+  const hbsObject = {
+    listItem: listItems,
+    Item: items
+  };
+
+  console.table(hbsObject);
+
+  res.render("index.handlebars", hbsObject);
+}); 
 
 module.exports = router;
